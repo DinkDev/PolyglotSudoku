7ffd337d1986c60bc8e722aa0c97a51c4484f6a4
DinkDev <dinkhome@hotmail.com>
2020-03-29 13:34:13 -0400

    Transformed Puzzle into a real class containing the actual puzzle being worked on by PuzzleSolver.

--- SudokuPy/Puzzle.py
+++ SudokuPy/Puzzle.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def cross(list_a, list_b):
     """Cross product of elements in A and elements in B."""
     return [a + b for a in list_a for b in list_b]
@@ -20,99 +23,28 @@ class Puzzle:
 
     def __init__(self):
         super().__init__()
+
         self.digits = '123456789'
         self.rows = ['r' + d for d in self.digits]
         self.cols = ['c' + d for d in self.digits]
         self.squares = cross(self.rows, self.cols)
-        self.unit_list = ([cross(self.rows, [c]) for c in self.cols]
-                          + [cross([r], self.cols) for r in self.rows]
-                          + [cross(rs, cs) for rs in (['r1', 'r2', 'r3'], ['r4', 'r5', 'r6'], ['r7', 'r8', 'r9'])
-                             for cs in (['c1', 'c2', 'c3'], ['c4', 'c5', 'c6'], ['c7', 'c8', 'c9'])])
-        self.units = dict((s, [u for u in self.unit_list if s in u]) for s in self.squares)
-        self.peers = dict((s, set(sum(self.units[s], [])) - {s}) for s in self.squares)
+        self.grid = dict((s, None) for s in self.squares)
 
-    def load_puzzle(self, grid):
-        """Convert grid to a dict of possible values, {square: digits}, or
-        return False if a contradiction is detected."""
-        # To start, every square can be any digit; then assign values from the grid.
-        values = dict((s, self.digits) for s in self.squares)
-        for s, d in self.load_values(grid).items():
-            if d in self.digits and not self.assign(values, s, d):
-                return False  # (Fail if we can't assign d to square s.)
-        return values
-
-    def load_values(self, grid):
-        """Convert grid into a dict of {square: char} with '0' or '.' for empties."""
+    def load_puzzle(self, definition):
+        """Convert grid into a dict of {square: char} with '.', ' ' or '0' for empties."""
 
         # convert list of strings to string
-        if isinstance(grid, list):
+        if isinstance(definition, list):
             flat_grid = ''
-            for sub_grid in grid:
+            for sub_grid in definition:
                 for item in sub_grid:
                     flat_grid += item
-            grid = flat_grid
+            definition = flat_grid
 
-        chars = [c for c in grid if c in self.digits or c in '0.']
+        chars = [c if c in self.digits else None for c in definition]
+
         if len(chars) != 81:
-            print(grid, chars, len(chars))
+            print(definition, chars, len(chars))
         assert len(chars) == 81
-        return dict(zip(self.squares, chars))
-
-    def assign(self, values, s, d):
-        """Eliminate all the other values (except d) from values[s] and propagate.
-        Return values, except return False if a contradiction is detected."""
-        other_values = values[s].replace(d, '')
-        if all(self.eliminate(values, s, d2) for d2 in other_values):
-            return values
-        else:
-            return False
-
-    def eliminate(self, values, s, d):
-        """Eliminate d from values[s]; propagate when values or places <= 2.
-        Return values, except return False if a contradiction is detected."""
-        if d not in values[s]:
-            return values  # Already eliminated
-        values[s] = values[s].replace(d, '')
-        # (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-        if len(values[s]) == 0:
-            return False  # Contradiction: removed last value
-        elif len(values[s]) == 1:
-            d2 = values[s]
-            if not all(self.eliminate(values, s2, d2) for s2 in self.peers[s]):
-                return False
-        # (2) If a unit u is reduced to only one place for a value d, then put it there.
-        for u in self.units[s]:
-            dplaces = [s for s in u if d in values[s]]
-            if len(dplaces) == 0:
-                return False  # Contradiction: no place for this value
-            elif len(dplaces) == 1:
-                # d can only be in one place in unit; assign it there
-                if not self.assign(values, dplaces[0], d):
-                    return False
-        return values
-
-    def search(self, values):
-        """Using depth-first search and propagation, try all possible values."""
-        if values is False:
-            return False  # Failed earlier
-        if all(len(values[s]) == 1 for s in self.squares):
-            return values  # Solved!
-        # Chose the unfilled square s with the fewest possibilities
-        n, s = min((len(values[s]), s) for s in self.squares if len(values[s]) > 1)
-        for d in values[s]:
-            result = self.search(self.assign(values.copy(), s, d))
-            if result:
-                return result
-
-    def solve(self, grid):
-        return self.search(self.load_puzzle(grid))
-
-    def display(self, values):
-        """Display these values as a 2-D grid."""
-        width = 1 + max(len(values[s]) for s in self.squares)
-        line = '+'.join(['-' * (width * 3)] * 3)
-        for r in self.rows:
-            print(''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in self.cols))
-            if r in 'CF':
-                print(line)
-        print()
+        for s, d in dict(zip(self.squares, chars)).items():
+            self.grid[s] = d
--- SudokuPy/test_puzzle.py
+++ SudokuPy/test_puzzle.py
@@ -1,69 +1,81 @@
 from Puzzle import Puzzle
+from PuzzleSolver import PuzzleSolver
 import time
 
 
-def test_init():
-    """A set of tests that must pass."""
+def test_puzzle_init():
     p = Puzzle()
-
     assert len(p.squares) == 81
-    assert len(p.unit_list) == 27
-    assert all(len(p.units[s]) == 3 for s in p.squares)
-    assert all(len(p.peers[s]) == 20 for s in p.squares)
-
-    assert p.units['r3c2'] == [['r1c2', 'r2c2', 'r3c2', 'r4c2', 'r5c2', 'r6c2', 'r7c2', 'r8c2', 'r9c2'],
-                               ['r3c1', 'r3c2', 'r3c3', 'r3c4', 'r3c5', 'r3c6', 'r3c7', 'r3c8', 'r3c9'],
-                               ['r1c1', 'r1c2', 'r1c3', 'r2c1', 'r2c2', 'r2c3', 'r3c1', 'r3c2', 'r3c3']]
-
-    assert p.peers['r3c2'] == {'r1c2', 'r2c2', 'r4c2', 'r5c2', 'r6c2', 'r7c2', 'r8c2', 'r9c2', 'r3c1', 'r3c3', 'r3c4',
-                               'r3c5', 'r3c6', 'r3c7', 'r3c8', 'r3c9', 'r1c1', 'r1c3', 'r2c1', 'r2c3'}
-
-    print('All tests pass.')
 
 
-def solve_all(grids, name=''):
+def test_puzzle_solver_init():
+    puzzle = Puzzle()
+    solver = PuzzleSolver(puzzle)
+    assert len(solver.unit_list) == 27
+    assert all(len(solver.units[s]) == 3 for s in puzzle.squares)
+    assert all(len(solver.peers[s]) == 20 for s in puzzle.squares)
+
+    assert solver.units['r3c2'] == [['r1c2', 'r2c2', 'r3c2', 'r4c2', 'r5c2', 'r6c2', 'r7c2', 'r8c2', 'r9c2'],
+                                    ['r3c1', 'r3c2', 'r3c3', 'r3c4', 'r3c5', 'r3c6', 'r3c7', 'r3c8', 'r3c9'],
+                                    ['r1c1', 'r1c2', 'r1c3', 'r2c1', 'r2c2', 'r2c3', 'r3c1', 'r3c2', 'r3c3']]
+
+    assert solver.peers['r3c2'] == {'r1c2', 'r2c2', 'r4c2', 'r5c2', 'r6c2', 'r7c2', 'r8c2', 'r9c2', 'r3c1', 'r3c3',
+                                    'r3c4', 'r3c5', 'r3c6', 'r3c7', 'r3c8', 'r3c9', 'r1c1', 'r1c3', 'r2c1', 'r2c3'}
+
+
+def solve_all(definitions, name=''):
     """Attempt to solve a sequence of grids. Report results."""
-    times, results = zip(*[time_solve(grid) for grid in grids])
+
+    times, results = zip(*[time_solve(definition) for definition in definitions])
     n = len(results)
     if n > 1:
         print("Solved %d of %d %s puzzles (avg %.2f secs (%d Hz), max %.2f secs)." % (
             sum(results), n, name, sum(times) / n, n / sum(times), max(times)))
 
 
-def time_solve(grid):
+def time_solve(definition):
     p = Puzzle()
     start = time.perf_counter()
-    values = p.solve(grid)
+
+    p.load_puzzle(definition)
+
+    s = PuzzleSolver(p)
+    values = s.solve()
     t = time.perf_counter() - start
-    return t, solved(p, values)
+    return t, solved(p, s, values)
 
 
-def solved(p, values):
+def solved(puzzle: Puzzle, solver: PuzzleSolver, values):
     """A puzzle is solved if each unit is a permutation of the digits 1 to 9."""
 
-    def unit_solved(unit): return set(values[s] for s in unit) == set(p.digits)
+    def unit_solved(unit): return set(values[s] for s in unit) == set(puzzle.digits)
 
-    return values is not False and all(unit_solved(unit) for unit in p.unit_list)
+    return values is not False and all(unit_solved(unit) for unit in solver.unit_list)
 
 
 grid1 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+grid1b = '  3 2 6  9  3 5  1  18 64    81 29  7       8  67 82    26 95  8  2 3  9  5 1 3  '
 grid2 = '4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......'
 grid2d = [
-            '4.....8.5',
-            '.3.......',
-            '...7.....',
-            '.2.....6.',
-            '....8.4..',
-            '....1....',
-            '...6.3.7.',
-            '5..2.....',
-            '1.4......',
-        ]
+    '4.....8.5',
+    '.3.......',
+    '...7.....',
+    '.2.....6.',
+    '....8.4..',
+    '....1....',
+    '...6.3.7.',
+    '5..2.....',
+    '1.4......',
+]
 hard1 = '.....6....59.....82....8....45........3........6..3.54...325..6..................'
 
 
 def test_grid1():
     solve_all([grid1], "grid1")
+
+
+def test_grid1b():
+    solve_all([grid1b], "grid1b")
 
 
 def test_grid2():
@@ -79,12 +91,18 @@ def test_hard1():
 
 
 def test_solve_easy():
-    solve_all(open("sudoku-easy50.txt"), "easy")
+    with open('sudoku-easy50.txt') as f:
+        easy_puzzles = [line.rstrip('\n') for line in f]
+        solve_all(easy_puzzles, "easy")
 
 
 def test_solve_hard():
-    solve_all(open("sudoku-top95.txt"), "hard")
+    with open('sudoku-top95.txt') as f:
+        hard_puzzles = [line.rstrip('\n') for line in f]
+        solve_all(hard_puzzles, "hard")
 
 
 def test_solve_hardest():
-    solve_all(open("sudoku-hardest.txt"), "hardest")
+    with open('sudoku-hardest.txt') as f:
+        hardest_puzzles = [line.rstrip('\n') for line in f]
+        solve_all(hardest_puzzles, "hardest")
