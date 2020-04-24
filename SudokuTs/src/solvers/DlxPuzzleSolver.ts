8da47309084ab22590caca1556cd4cfbf290b890
DinkDev <dinkhome@hotmail.com>
2020-04-24 06:56:50 -0400

    Interim check in of TypeScript DLX implementation and tests.

--- SudokuTs/src/solvers/DlxPuzzleSolver.ts
+++ SudokuTs/src/solvers/DlxPuzzleSolver.ts
@@ -4,65 +4,99 @@ import { PuzzleSize, PuzzleSizeUtil } from '../PuzzleSize';
 import { product2, product3, range } from './IterationUtils';
 
 export class DlxPuzzleSolver {
-  public puzzle: Puzzle;
+    public puzzle: Puzzle;
 
-  private x: Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>
-    = new Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>();
-  private y: Collections.Dictionary<[string, string], Array<[string, [number, number]]>>
-    = new Collections.Dictionary<[string, string], Array<[string, [number, number]]>>();
+    private x: Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>
+        = new Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>();
+    private y: Collections.Dictionary<[string, string], Array<[string, [number, number]]>>
+        = new Collections.Dictionary<[string, string], Array<[string, [number, number]]>>();
 
-  constructor(puzzle: Puzzle) {
-    this.puzzle = puzzle;
+    constructor(puzzle: Puzzle) {
+        this.puzzle = puzzle;
 
-    const constraintTypes: string[] = ['cell', 'row', 'column', 'box'];
-    const valueRange: number[] = range(1, this.puzzle.puzzleSize + 1);
-    constraintTypes.forEach(constraint => {
-      product2(valueRange, valueRange).forEach(pair => {
-        this.x.setValue([constraint, pair], new Collections.Set<[string, string]>());
-      });
-    });
+        const constraintTypes: string[] = ['cell', 'row', 'column', 'box'];
+        const valueRange: number[] = range(1, this.puzzle.puzzleSize + 1);
+        constraintTypes.forEach(constraint => {
+            product2(valueRange, valueRange).forEach(pair => {
+                this.x.setValue([constraint, pair], new Collections.Set<[string, string]>());
+            });
+        });
 
-    const boxSize: number = Math.sqrt(this.puzzle.puzzleSize);
-    product3(valueRange, valueRange, valueRange).forEach(triple => {
-      const [r, c, n] = triple;
+        const boxSize: number = Math.sqrt(this.puzzle.puzzleSize);
+        product3(valueRange, valueRange, valueRange).forEach(triple => {
+            const [r, c, n] = triple;
 
-      const yKey: [string, string] = [`r${r}c${c}`, `${n}`];
-      const b = Math.floor((r - 1) / boxSize) * boxSize + Math.floor((c - 1) / boxSize) + 1;
+            const yKey: [string, string] = [`r${r}c${c}`, `${n}`];
+            const b = Math.floor((r - 1) / boxSize) * boxSize + Math.floor((c - 1) / boxSize) + 1;
 
-      const yValue: Array<[string, [number, number]]>
-          = [['cell', [r, c]], ['row', [r, n]], ['column', [c, n]], ['box', [b, n]]];
+            const yValue: Array<[string, [number, number]]>
+                = [['cell', [r, c]], ['row', [r, n]], ['column', [c, n]], ['box', [b, n]]];
 
-      this.y.setValue(yKey, yValue);
-    });
+            this.y.setValue(yKey, yValue);
+        });
 
-    this.y.keys().forEach(i => {
-      const row = this.y.getValue(i);
-      row?.forEach(j => {
-        this.x.getValue(j)?.add(i);
-      });
-    });
+        this.y.keys().forEach(i => {
+            const row = this.y.getValue(i);
+            row?.forEach(j => {
+                this.x.getValue(j)?.add(i);
+            });
+        });
 
-    // load the puzzle
-    this.puzzle.grid.keys().forEach(i => {
-      const v = this.puzzle.grid.getValue(i);
-      if (v !== null && v !== undefined) {
-        this.cover(this.x, this.y, [i, v]);
-      }
-    });
-  }
+        // load the puzzle
+        this.puzzle.grid.keys().forEach(i => {
+            const v = this.puzzle.grid.getValue(i);
+            if (v !== null && v !== undefined) {
+                this.cover(this.x, this.y, [i, v]);
+            }
+        });
+    }
 
-  private cover(
-    x: Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>,
-    y: Collections.Dictionary<[string, string], Array<[string, [number, number]]>>,
-    r: [string, string]): Array<Collections.Set<[string, string]>> {
-    throw new Error("Method not implemented.");
+    private cover(
+        x: Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>,
+        y: Collections.Dictionary<[string, string], Array<[string, [number, number]]>>,
+        r: [string, string]): Array<Collections.Set<[string, string]>> {
 
-    const cols: Array<Collections.Set<[string, string]>> = [];
-    y.getValue(r)?.forEach(j =>{
+        const cols: Array<Collections.Set<[string, string]>> = [];
+        y.getValue(r)?.forEach(j => {
+            x.getValue(j)?.forEach(i => {
+                y.getValue(i)?.forEach(k => {
+                    if (k !== j) {
+                        x.getValue(k)?.remove(i);
+                    }
 
-      // TODO: implement cover, and all the rest
+                    const v = x.getValue(j);
+                    x.remove(j);
+                    if (v !== undefined) {
+                        cols.push(v);
+                    }
+                });
+            });
+        });
 
-    });
-  }
+        return cols;
+    }
 
+    private uncover(
+        x: Collections.Dictionary<[string, [number, number]], Collections.Set<[string, string]>>,
+        y: Collections.Dictionary<[string, string], Array<[string, [number, number]]>>,
+        r: [string, string],
+        cols: Array<Collections.Set<[string, string]>>) {
+
+        y.getValue(r)?.slice().reverse().forEach(j => {
+            const v = cols.pop();
+            if (v !== undefined) {
+                x.setValue(j, v);
+            }
+
+            x.getValue(j)?.forEach(i => {
+                y.getValue(i)?.forEach(k => {
+                    if (k !== j) {
+                        x.getValue(k)?.add(i);
+                    }
+                });
+            });
+        });
+
+        return cols;
+    }
 }
---
+++ SudokuTs/test/DlxPuzzleSolverTests.ts
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import { Puzzle } from '../src/Puzzle';
+import { PuzzleSize } from '../src/PuzzleSize';
+import { DlxPuzzleSolver } from '../src/solvers/DlxPuzzleSolver';
+
+describe('DlxPuzzleSolver constructor', () => {
+
+    it('args instanceOf Puzzle initializes state', () => {
+        const boardDef = [
+            '4..27.6..',
+            '798156234',
+            '.2.84...7',
+            '237468951',
+            '849531726',
+            '561792843',
+            '.82.15479',
+            '.7..243..',
+            '..4.87..2',
+        ];
+        const puzzle = new Puzzle(9);
+        puzzle.loadPuzzle(boardDef);
+
+        const sut = new DlxPuzzleSolver(puzzle);
+
+        expect(sut.puzzle).to.equal(puzzle);
+        // tslint:disable-next-line: no-string-literal
+        expect(sut['x'].size()).to.equal(141);
+        // tslint:disable-next-line: no-string-literal
+        expect(sut['y'].size()).to.equal(729);
+        // expect(sut.puzzleSize).to.equal(PuzzleSize.NineByNine);
+        // expect(sut.grid.getValue('r0c0')).to.equal('4');
+        // expect(sut.grid.getValue('r0c1')).to.equal(null);
+        // expect(sut.grid.getValue('r4c4')).to.equal('3');
+        // expect(sut.grid.getValue('r8c8')).to.equal('2');
+    });
+
+});
